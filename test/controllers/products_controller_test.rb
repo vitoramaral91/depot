2f310cc2c5991c2ed09eebbb51569e7edfbfa44f
vitoramaral91 <vvdda91@gmail.com>
2021-08-09 20:10:16 -0300

    'Validation!'

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -3,6 +3,8 @@ require "test_helper"
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   setup do
     @product = products(:one)
+    @title = "The Great Book #{rand(1000)}"
+
   end
 
   test "should get index" do
@@ -17,7 +19,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
 
   test "should create product" do
     assert_difference('Product.count') do
-      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+      post products_url, params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     end
 
     assert_redirected_to product_url(Product.last)
@@ -34,7 +36,7 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @product.title } }
+    patch product_url(@product), params: { product: { description: @product.description, image_url: @product.image_url, price: @product.price, title: @title } }
     assert_redirected_to product_url(@product)
   end
 
@@ -44,5 +46,23 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     end
 
     assert_redirected_to products_url
+  end
+end
+
+def create
+  @product = Product.new(product_params)
+  
+  respond_to do |format|
+    if @product.save
+      format.html { redirect_to @product,
+        notice: 'Product was successfully created.' }
+      format.json { render :show, status: :created,
+        location: @product }
+    else
+      puts @product.errors.full_messages
+      format.html { render :new }
+      format.json { render json: @product.errors,
+        status: :unprocessable_entity }
+    end
   end
 end
